581f2ccfc0a52090b85494cb7631cc2900e47b49
Sabariramamoorthy <86560376+Sabariramamoorthy@users.noreply.github.com>
2021-07-25 01:21:05 +0530

    save

--- js/index.js
+++ js/index.js
@@ -1,57 +1,16 @@
 function page(id){
-
-    if (id==1){
-        window.location.href="#page1"
-        document.getElementById("page1").style.visibility="visible";
-        document.getElementById("page1").style.display="block";
-        document.getElementById("page2").style.visibility="hidden";
-        document.getElementById("page2").style.display="none";
-        document.getElementById("page3").style.visibility="hidden";
-        document.getElementById("page3").style.display="none";
-        document.getElementById("page4").style.visibility="hidden";
-        document.getElementById("page4").style.display="none";
-    }
-
-    if (id==2){
-        console.log(id);
-        window.location.href="#page2"
-        document.getElementById("page2").style.visibility="visible";
-        document.getElementById("page2").style.display="block";
-        document.getElementById("page1").style.visibility="hidden";
-        document.getElementById("page1").style.display="none";
-        document.getElementById("page3").style.visibility="hidden";
-        document.getElementById("page3").style.display="none";
-        document.getElementById("page4").style.visibility="hidden";
-        document.getElementById("page4").style.display="none";
+   let total=4;
+   for(let i=1;i<=total;i++){
+    if(id==i){
+        window.location.href="#page"+i.toString();
+        document.getElementById("page"+i.toString()).style.visibility="visible";
+        document.getElementById("page"+i.toString()).style.display="block";
 
     }
-
-    if (id==3){
-        console.log(id);
-        window.location.href="#page3"
-        document.getElementById("page3").style.visibility="visible";
-        document.getElementById("page3").style.display="block";
-        document.getElementById("page1").style.visibility="hidden";
-        document.getElementById("page1").style.display="none";
-        document.getElementById("page2").style.visibility="hidden";
-        document.getElementById("page2").style.display="none";
-        document.getElementById("page4").style.visibility="hidden";
-        document.getElementById("page4").style.display="none";
-
-
+    else{
+        document.getElementById("page"+i.toString()).style.visibility="hidden";
+        document.getElementById("page"+i.toString()).style.display="none";
     }
+}
 
-    if (id==4){
-        console.log(id);
-        window.location.href="#page4"
-        document.getElementById("page4").style.visibility="visible";
-        document.getElementById("page4").style.display="block";
-        document.getElementById("page1").style.visibility="hidden";
-        document.getElementById("page1").style.display="none";
-        document.getElementById("page2").style.visibility="hidden";
-        document.getElementById("page2").style.display="none";
-        document.getElementById("page3").style.visibility="hidden";
-        document.getElementById("page3").style.display="none";
-
-    }
 }
