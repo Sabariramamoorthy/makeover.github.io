401adb66d2609284af49154035e7b18f64c2d0ac
Sabariramamoorthy <86560376+Sabariramamoorthy@users.noreply.github.com>
2021-07-25 00:48:27 +0530

    save

--- js/index.js
+++ js/index.js
@@ -6,6 +6,10 @@ function page(id){
         document.getElementById("page1").style.display="block";
         document.getElementById("page2").style.visibility="hidden";
         document.getElementById("page2").style.display="none";
+        document.getElementById("page3").style.visibility="hidden";
+        document.getElementById("page3").style.display="none";
+        document.getElementById("page4").style.visibility="hidden";
+        document.getElementById("page4").style.display="none";
     }
 
     if (id==2){
@@ -15,5 +19,39 @@ function page(id){
         document.getElementById("page2").style.display="block";
         document.getElementById("page1").style.visibility="hidden";
         document.getElementById("page1").style.display="none";
+        document.getElementById("page3").style.visibility="hidden";
+        document.getElementById("page3").style.display="none";
+        document.getElementById("page4").style.visibility="hidden";
+        document.getElementById("page4").style.display="none";
+
+    }
+
+    if (id==3){
+        console.log(id);
+        window.location.href="#page3"
+        document.getElementById("page3").style.visibility="visible";
+        document.getElementById("page3").style.display="block";
+        document.getElementById("page1").style.visibility="hidden";
+        document.getElementById("page1").style.display="none";
+        document.getElementById("page2").style.visibility="hidden";
+        document.getElementById("page2").style.display="none";
+        document.getElementById("page4").style.visibility="hidden";
+        document.getElementById("page4").style.display="none";
+
+
+    }
+
+    if (id==4){
+        console.log(id);
+        window.location.href="#page4"
+        document.getElementById("page4").style.visibility="visible";
+        document.getElementById("page4").style.display="block";
+        document.getElementById("page1").style.visibility="hidden";
+        document.getElementById("page1").style.display="none";
+        document.getElementById("page2").style.visibility="hidden";
+        document.getElementById("page2").style.display="none";
+        document.getElementById("page3").style.visibility="hidden";
+        document.getElementById("page3").style.display="none";
+
     }
 }
